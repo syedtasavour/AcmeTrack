2895eaf9cff00ecfa61b34e9f8e436a4d6019c3e
Syed Tasavour <syedtasavour86@gmail.com>
2025-05-16 14:41:09 +0530

    chore: remove console log for environment variables in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,13 +4,12 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 dotenv.config();
 const app = express();
-console.log("Environment Variables Loaded", process.env.CORS_ORIGIN),
-  app.use(
-    cors({
-      origin: process.env.CORS_ORIGIN, // your frontend URL
-      credentials: true, // allow cookies to be sent
-    })
-  );
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN, // your frontend URL
+    credentials: true, // allow cookies to be sent
+  })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
